e1809f08d96c5fac66e340810d3831c86321edc5
Coby Lu <lucoby@gmail.com>
2015-04-03 23:08:44 -0400

    Initial commit

---
+++ src/com/coby/talentbuddy/TweetsPerSecond.java
@@ -0,0 +1,15 @@
+package com.coby.talentbuddy;
+
+
+
+public class TweetsPerSecond {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
